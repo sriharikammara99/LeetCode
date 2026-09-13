a8738e1da4d92b327f140fcb42e2b9e74bd771ba
Sri Hari <srihariachari2005@gmail.com>
2026-09-13 20:29:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.30%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long l=1,r=x,ans=0;
+        while(l<=r){ long long m=(l+r)/2; if(m*m<=x) ans=m,l=m+1; else r=m-1; }
+        return ans;
+    }
+};
